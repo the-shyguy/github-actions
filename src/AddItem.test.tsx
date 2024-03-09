c5c476152798b4a2f4dba9f44b6667dfffe3e0cc
Rohit Singh <rohit14kvs@gmail.com>
2024-03-09 12:31:52 +0530

    initial commit

--- src/AddItem.test.tsx
+++ src/AddItem.test.tsx
@@ -1,14 +1,14 @@
-import { render, screen } from "@testing-library/react";
-import AddItem from "../src/AddItem";
+import { render, screen } from '@testing-library/react'
+import AddItem from '../src/AddItem'
 
-const empty = () => "";
+const empty = () => ''
 
-test("Renders Input Form", () => {
-  render(<AddItem addItem={empty} />);
+test('Renders Input Form', () => {
+  render(<AddItem addItem={empty} />)
 
-  const taskElement = screen.getByText("Task:");
-  expect(taskElement).toBeInTheDocument();
+  const taskElement = screen.getByText('Task:')
+  expect(taskElement).toBeInTheDocument()
 
-  const priorityElement = screen.getByText("Priority:");
-  expect(priorityElement).toBeInTheDocument();
-});
+  const priorityElement = screen.getByText('Priority:')
+  expect(priorityElement).toBeInTheDocument()
+})
